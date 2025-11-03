0c0b97f4cb83db75023bfabfcfe8e9e128816833
Vivo Alyanna Mae <vivoalyannamae_bsit@plmun.edu.ph>
2025-11-03 18:57:32 +0800

    Create IfElseGrade.cpp

---
+++ IfElseGrade.cpp
@@ -0,0 +1,19 @@
+// Vivo, Alyanna Mae R.
+// BSIT-2J
+// LAB EXERCISES 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int grade;
+    cout << "Enter grade: ";
+    cin >> grade;
+
+    if (grade >= 75)
+        cout << "Result: Pass" << endl;
+    else
+        cout << "Result: Fail" << endl;
+
+    return 0;
+}
