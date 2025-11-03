4427aebc75a108ed31d7680e53c464130e3df277
Vivo Alyanna Mae <vivoalyannamae_bsit@plmun.edu.ph>
2025-11-03 19:07:59 +0800

    Create Lab 5 - NestedIfGrade.cpp

---
+++ Lab 5 - NestedIfGrade.cpp
@@ -0,0 +1,20 @@
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int grade;
+    cout << "Enter grade: ";
+    cin >> grade;
+
+    if (grade >= 90)
+        cout << "Remark: Excellent" << endl;
+    else if (grade >= 85)
+        cout << "Remark: Very Good" << endl;
+    else if (grade >= 75)
+        cout << "Remark: Satisfactory" << endl;
+    else
+        cout << "Remark: Fail" << endl;
+
+    return 0;
+}
