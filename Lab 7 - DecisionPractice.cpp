34d91dd5d750c6d784a2aad720cdb9dd4496f31c
Vivo Alyanna Mae <vivoalyannamae_bsit@plmun.edu.ph>
2025-11-03 19:21:13 +0800

    Create Lab 7 - DecisionPractice.cpp

---
+++ Lab 7 - DecisionPractice.cpp
@@ -0,0 +1,35 @@
+// Vivo, Alyanna Mae R.
+// BSIT - 2J
+// LAB EXERCISES 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int grade, course;
+    cout << "Enter grade: ";
+    cin >> grade;
+    cout << "Enter course code (1–3): ";
+    cin >> course;
+
+    if (grade >= 75)
+        cout << "Result: Pass" << endl;
+    else
+        cout << "Result: Fail" << endl;
+
+    switch (course) {
+        case 1:
+            cout << "Course: Programming 1" << endl;
+            break;
+        case 2:
+            cout << "Course: Data Structures" << endl;
+            break;
+        case 3:
+            cout << "Course: Algorithms" << endl;
+            break;
+        default:
+            cout << "Invalid course code." << endl;
+    }
+
+    return 0;
+}
